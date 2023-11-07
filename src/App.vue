a41037f5dedcfb73250d9f1a97d59a8fe74399c2
marvin <marvin010528@foxmail.com>
2023-11-07 11:44:28 +0800

    fix: postcss插件报错解决

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,7 @@ logger("aaa");
 </script>
 
 <template>
-  <h1 class="text-3xl text-amber-400 font-bold underline">Hello world!</h1>
+  <h1 class="text-3xl bg-sky-500 text-amber-100 font-bold underline">
+    Hello world!
+  </h1>
 </template>
