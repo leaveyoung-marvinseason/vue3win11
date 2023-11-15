56b34f54dee62850c39435150a88aabfb57b5db7
marvin <marvin010528@foxmail.com>
2023-11-15 18:13:10 +0800

    fix: tailwind脚本嵌入

--- src/components/icon/index.vue
+++ src/components/icon/index.vue
@@ -2,26 +2,29 @@
   Author: marvin-season
   Created: 2023-11
 -->
-<script setup lang='ts'>
-import { NImage } from 'naive-ui';
-import { onBeforeMount, ref } from 'vue';
-const props = withDefaults(defineProps<{
-    name: string,
-    size?: number
-}>(), {
-    size: 40
-})
+<script setup lang="ts">
+import { NImage } from "naive-ui";
+import { onBeforeMount, ref } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    name: string;
+    size?: number;
+  }>(),
+  {
+    size: 40,
+  },
+);
 
 const emit = defineEmits<{
-    click: []
-}>()
-
+  click: [];
+}>();
 </script>
- 
+
 <template>
-  <svg :height="size" :width="size">
+  <svg :height="size" :width="size" @click="emit('click')">
     <use :xlink:href="`#${name}`" :height="size" :width="size"></use>
   </svg>
 </template>
- 
+
 <style scoped></style>
