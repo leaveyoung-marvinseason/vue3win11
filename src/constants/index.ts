bd8659f989f9e767326c8c033aff649d7709a5e8
marvin <marvin010528@foxmail.com>
2023-11-24 17:49:04 +0800

    feat:app拖拽布局

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,6 @@ export const APPS = 'APP';
 export const BAR_APPS = 'BAR_APP';
 export const FOCUS_DIALOG_Z_INDEX = 99
 export const UN_FOCUS_DIALOG_Z_INDEX = 10
+
+export const APP_WIDTH = 2
+export const APP_HEIGHT = 2
