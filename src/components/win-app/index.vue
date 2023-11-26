82570bc10b658a941a64fcd8c393612bc7eef373
marvin <marvin010528@foxmail.com>
2023-11-26 15:39:33 +0800

    feat:app dbclick

--- src/components/win-app/index.vue
+++ src/components/win-app/index.vue
@@ -25,8 +25,8 @@ const handleClick = () => {
  
 <template>
   <div class="flex w-20 flex-col items-center gap-2 inline-block ">
-    <Icon class="cursor-pointer" @click="handleClick" :name="props.app.icon" :size="props.size"/>
-    <span class="text-white text-xs break-all" v-if="props.app.label">{{ props.app.label }}</span>
+    <Icon class="cursor-pointer" @dblclick="handleClick" :name="props.app.icon" :size="props.size"/>
+    <span class="text-white text-xs break-all" @dblclick="handleClick" v-if="props.app.label">{{ props.app.label }}</span>
   </div>
 </template>
  
